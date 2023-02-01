d7c7d1331a361e76be666b33723a10e2129cdcc2
13 <eggerben@gmail.com>
2023-02-01 14:32:15 +0100

    fixed timezone to Rome

--- src/main.cpp
+++ src/main.cpp
@@ -86,7 +86,8 @@ void setup()
     drawString(4, 0, "WIFI", LEFT);
 
     // Time
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    // configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
+    configTzTime(time_zone, ntpServer1, ntpServer2);
     printLocalTime();
 
     // FetchJson
@@ -416,32 +417,7 @@ void printLocalTime()
     return;
   }
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  Serial.print("Day of week: ");
-  Serial.println(&timeinfo, "%A");
-  Serial.print("Month: ");
-  Serial.println(&timeinfo, "%B");
-  Serial.print("Day of Month: ");
-  Serial.println(&timeinfo, "%d");
-  Serial.print("Year: ");
-  Serial.println(&timeinfo, "%Y");
-  Serial.print("Hour: ");
-  Serial.println(&timeinfo, "%H");
-  Serial.print("Hour (12 hour format): ");
-  Serial.println(&timeinfo, "%I");
-  Serial.print("Minute: ");
-  Serial.println(&timeinfo, "%M");
-  Serial.print("Second: ");
-  Serial.println(&timeinfo, "%S");
-
-  Serial.println("Time variables");
-  char timeHour[3];
-  strftime(timeHour, 3, "%H", &timeinfo);
-  Serial.println(timeHour);
-  char timeWeekDay[10];
-  strftime(timeWeekDay, 10, "%A", &timeinfo);
-  Serial.println(timeWeekDay);
-  Serial.println();
-
+  
   char timeBuff[6];
   strftime(timeBuff, sizeof(timeBuff), "%H:%M", &timeinfo);
   String ret = timeBuff;
