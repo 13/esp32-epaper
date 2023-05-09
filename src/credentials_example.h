31dd94cb84b1e6602d8c61b432b396a238a7cf0c
13 <eggerben@gmail.com>
2023-05-09 18:34:17 +0200

    update credentials

--- src/credentials_example.h
+++ src/credentials_example.h
@@ -21,8 +21,8 @@ const char* mqtt_pass = "";
 const char* mqtt_server = "192.168.22.5";
 uint16_t mqtt_port = 1883;
 const char* mqtt_topics[] = { "shellies/HZ_DG/status/switch:0", 
-                              "sensors/22/json",
-                              "sensors/3f/json" };
+                              "muh/sensors/22/json",
+                              "muh/sensors/3f/json" };
 // Fetch URLs
 const char* http_urls[] = { "http://192.168.22.70/rpc/Shelly.GetStatus" };
 
